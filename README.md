8392706d42fb0ffb0d148fe934a4f9a95747b5e6
Garry Lachman <garry@lachman.co>
2016-07-09 22:42:45 +0300

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+"P2P Proxies Network - IP Authorizer
+
+Utils for autorize your IP in your rev.proxies.online account
+
+Require Python 3.5 +
+
+
+install:
+```
+pip install P2PProxiesAuthorizer
+```
+
+usage:
+```
+python -m P2PProxiesAuthorizer -e "your@email" -p "your password" 
+```
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='P2PProxiesAuthorizer',
+    version='0.0.1',
+    packages=['P2PProxiesAuthorizer'],
+    url='https://rev.proxies.online',
+    license='',
+    author='Garry Lachman',
+    author_email='garry@lachman.co',
+    description='P2P Proxies Authorizer',
+    install_requires=['urllib3']
+)
